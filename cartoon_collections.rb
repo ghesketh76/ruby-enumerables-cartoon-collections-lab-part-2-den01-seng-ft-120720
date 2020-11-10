e2a6214cf54f4c2fd3a566b5dd2bddd518fa647b
Grant Hesketh <ghesketh76@gmail.com>
2020-11-10 20:06:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -32,6 +32,6 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   
   planeteer_calls.find do |call|
-    call.find
+    call.include? (valid_calls)
   
 end
