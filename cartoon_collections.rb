9639b5a86f524c1d258764154256836e0e2e6e99
Grant Hesketh <ghesketh76@gmail.com>
2020-11-10 19:58:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,9 +15,10 @@ end
 def long_planeteer_calls(planeteer_calls)
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
-  
+  answer = false
   planeteer_calls.each do |call|
     call.length > 4
+    
     
   
 end
